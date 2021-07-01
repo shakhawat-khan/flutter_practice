1a407d00d7a8449d25610e338abb5612c0d9d72d
shakhawat-khan <satanfromhelll666@gmail.com>
2021-07-01 23:03:52 +0600

    app bar

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+void main() {
+
+  return runApp(MaterialApp(
+    home: Scaffold(
+      appBar:  AppBar(
+        title: Text('1st try'),
+        backgroundColor: Colors.greenAccent,
+      ),
+    ),
+  ));
+
+}
+
+
+
+
