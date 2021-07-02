593a6e615e3fa02979d80b7058d49483f0ca2c3c
shakhawat-khan <satanfromhelll666@gmail.com>
2021-07-02 22:14:36 +0600

    column and container

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,22 @@ void main() {
         title: Text('1st try'),
         backgroundColor: Colors.greenAccent,
       ),
+      body:Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.yellowAccent,
+            ),
+            child: Text('here'),
+          ),
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.blueAccent,
+            ),
+            child: Text('here'),
+          ),
+        ],
+      )
     ),
   ));
 
